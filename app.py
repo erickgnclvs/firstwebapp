0767449bb16f5c42ab9ee9e348ba999262ed2a23
erickgnclvs <erickgoncalves5@hotmail.com>
2022-10-22 08:52:12 -0300

    add database

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+from datetime import datetime
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("name", "world")
-    return render_template("hello.html", name=name)
+if __name__ == '__main__':
+    app.run(debug=True)
